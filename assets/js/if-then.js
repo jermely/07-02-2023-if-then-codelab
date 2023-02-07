c9edb22b88501c46e9351c383f9e9392db1d84d6
jermely <62332835+jermely@users.noreply.github.com>
2023-02-07 22:54:03 +0100

    wip

--- assets/js/if-then.js
+++ assets/js/if-then.js
@@ -1,40 +1,36 @@
-
 // variabler du skal bruge i din kode...
 const A = 10;
 const B = 30;
-const C = '30';
+const C = "30";
 const D = 10;
 const E = 40;
 
 // Opgave 1: skriv et expression der viser true når A er lig med B.
-
-if ( /*..dit expression Her*/ ) {
-    console.log('statement true!');
+if (A === B) {
+  console.log("statement true!");
 }
 
-
 //opgave 2: skrive en If then statement der vurderer om A er større end B.
-
-
-
+if (A > B) {
+  console.log("done 2");
+}
 
 //opgave 3: Skriv en If then statement der vurderer om A er mindre end eller lig med B
-
-
-
-
+if (A <= B) {
+  console.log("done 3");
+}
 
 //opgave 4: Skriv en If then statement der bliver FALSE når du sammenligner B og C.
-
-
-
-
+if (B === C) {
+  console.log("done 4");
+}
 
 //opgave 5: Skriv en If then statement der bliver TRUE når du sammenligner B og C.
+if (B == C) {
+  console.log("done 5");
+}
 
-
-
-
-//opgave 6: Skriv en If then statement der sammenligner B = C og E = A hvis B=C skal den blive TRUE. 
-
-
+//opgave 6: Skriv en If then statement der sammenligner B = C og E = A hvis B=C skal den blive TRUE.
+if ((B == C && E == A) || B == C) {
+  console.log("done 6");
+}
--- assets/js/realworld.js
+++ assets/js/realworld.js
@@ -1,28 +1,22 @@
-
 /* du må både ændre koden her, og i din HTML, hvis det er nødvendigt.. */
-
 
 /*Opgave 1: skriv en validering af svar i koden herunder. resultatet skal vises i myAnsverFeedbackElement når bruger trykker på myAnsverButton .
 der et korrekte svar:
 nej 
 */
 
+const myAnsverInput = document.getElementById("answerOne");
+const myAnsverButton = document.getElementById("answerButton");
+const myAnsverFeedbackElement = document.getElementById("answerFeedback");
 
-const myAnsverInput = document.getElementById('answerOne');
-const myAnsverButton = document.getElementById('answerButton');
-const myAnsverFeedbackElement = document.getElementById('answerFeedback');
-
-myAnsverButton.addEventListener('click', () => {
-
-    /* din kode her.  husk at skrive view funktion der viser om svaret er korrekt i myAnsverFeedbackElement, brug evt. innerHTML*/
-
+myAnsverButton.addEventListener("click", () => {
+  /* din kode her.  husk at skrive view funktion der viser om svaret er korrekt i myAnsverFeedbackElement, brug evt. innerHTML*/
 });
 
 /*Opgave 2: udkommenter opgave 1, og omskriv nu din funktion til to korrekte svar:
 nej
 ja i disney film
 */
-
 
 /*opgave 3: skriv en kode der valider formen med følgende elementer: (denne opgave er på extreme niveau) 
 
@@ -32,17 +26,11 @@ ja i disney film
  du må gerne bare consol.logge de forskellige tests i konsollen, men hvis du kan må du gerne lave feedback i elementet formValideringResult.
 */
 
-
-
-
-
-
-
-
 /* helper functions*/
 function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+  var re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
 /* eks på kald:
